00711f503d20f5a09eae89f0b99c555710063ad6
Victor Tabacoff <victortabacoff2007@gmail.com>
2024-10-10 08:43:48 -0500

    Initial Commit

---
+++ FirebaseExample/FirebaseExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseExampleApp.swift
+//  FirebaseExample
+//
+//  Created by Victor B. Tabacoff on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FirebaseExampleTests/FirebaseExampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseExampleTests.swift
+//  FirebaseExampleTests
+//
+//  Created by Victor B. Tabacoff on 10/10/24.
+//
+
+import Testing
+@testable import FirebaseExample
+
+struct FirebaseExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
